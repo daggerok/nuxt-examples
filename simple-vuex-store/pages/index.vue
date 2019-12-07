1d94e364c4f971134ee8f7d2c4ed887efb7f7cce
Maksim Kostromin <daggerok@gmail.com>
2019-12-07 18:50:55 +0200

    restored deleted entities just to be sure (C) whatthecommit.com

--- simple-vuex-store/pages/index.vue
+++ simple-vuex-store/pages/index.vue
@@ -1,40 +1,33 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        simple
-      </h1>
-      <h2 class="subtitle">
-        My mind-blowing Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <button @click="incrementCounter"> + </button>
+    <button @click="decrementCounter"> - </button>
+    <button @click="resetCounter"> reset </button>
+    <div>{{counter}}</div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+// import Logo from '~/components/Logo.vue'
+import { mapMutations, mapState } from 'vuex';
+import {state} from "../store/counter";
 
 export default {
   components: {
-    Logo
-  }
+    // Logo
+  },
+  computed: {
+    ...mapState({
+      counter: state => state.counter.counter,
+    }),
+  },
+  methods: {
+    ...mapMutations({
+      incrementCounter: 'counter/increment',
+      decrementCounter: 'counter/decrement',
+      resetCounter    : 'counter/reset',
+    }),
+  },
 }
 </script>
 
---
+++ simple-vuex-store/store/counter.js
@@ -0,0 +1,9 @@
+export const state = () => ({
+  counter: 0,
+});
+
+export const mutations = {
+  increment: state => state.counter++,
+  decrement: state => state.counter--,
+  reset: state => state.counter = 0,
+};
